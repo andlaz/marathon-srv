3cface30edc2e76f4d9a8081e198e052622d73e4
Andras Szerdahelyi <andras.szerdahelyi@gmail.com>
2016-01-28 03:06:10 -0800

    More logging

--- lib/marathon/client.rb
+++ lib/marathon/client.rb
@@ -27,28 +27,28 @@ module Marathon
       @logger = Logger.new(STDOUT)
       @logger.level = options[:log_level] || Logger::ERROR
       
-      @logger.debug "Initialized with options %s" % options
+      @logger.debug "Initialized with options #{options}"
       
     end
     
     def get_bridged_port_array(application_id, healthy_tasks_only=false)
       
-      @logger.debug "Attempting to fetch application '%s' object from API, healthy_tasks_only: %s " % [application_id, healthy_tasks_only]
+      @logger.debug "Attempting to fetch application #{application_id} object from API, healthy_tasks_only: #{healthy_tasks_only} "
 
       api = URI::join(marathon_api, PATH_FIND_APP % application_id)
       Net::HTTP.new(api.host, api.port).start do |http|
         
         response = http.request Net::HTTP::Get.new api
-        @logger.debug "Received response: %s" % response
+        @logger.debug "Received response: #{response}"
         
         raise Marathon::InvalidResponseCodeError.new response.code unless response.code == "200"
         
         # parse JSON
         begin
-          @logger.debug "Parsing body '%s'" % response.body
+          @logger.debug "Parsing body #{response.body}"
           app = (JSON response.body)["app"]
           
-          @logger.debug "Retrieved app object %s" % app
+          @logger.debug "Retrieved app object #{app}"
           
           raise Marathon::NotDockerContainerizedApplicationError.new unless app["container"]["type"] == "DOCKER"
           raise Marathon::NoRunningTasksFoundError.new unless app["tasks"].size > 0
@@ -59,19 +59,21 @@ module Marathon
           app["tasks"].each do |task|
             
             if(healthy_tasks_only)
-              @logger.debug "Verifying health checks for task %s" % task
+              @logger.debug "Verifying health checks for task #{task}"
               # all health checks must be passing
               passing=true
               task["healthCheckResults"].each do |health_check_result|
                 (passing=false; @logger.debug "%s has failing health check, not considering it" % task; break) unless health_check_result["alive"] == true
                 
               end
-              
+              @logger.debug "All health checks passing - filtering ports for task #{task}"
               ports.push filter_ports(app, task) if passing
               
             else
               # just add task
+              @logger.debug "Ignoring health checks - filtering ports for task #{task}"
               ports.push filter_ports(app, task)
+              
             end
             
           end
